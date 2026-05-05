9a2a7f106f4af00ce3e99212fef7f4396817a1ab
Ubuntu <ubuntu@ip-172-31-23-112.ec2.internal>
2026-05-05 10:17:12 +0000

    clean repo

--- firsttest.py
+++ firsttest.py
@@ -1,35 +1,30 @@
-# vim test.py
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 options = Options()
 options.add_argument('--no-sandbox')
 options.add_argument('--headless')
 options.add_argument('--disable-dev-shm-usage')
 
-service = Service('/usr/bin/chromedriver')  # Explicit path to chromedriver
-driver = webdriver.Chrome(service=service, options=options)
-
-#driver = webdriver.Chrome(options=options)
+driver = webdriver.Chrome(options=options)
 
 driver.get("https://www.selenium.dev/selenium/web/web-form.html")
 
-title = driver.title
-assert title == "Web form"
+assert driver.title == "Web form"
 
-driver.implicitly_wait(5)
-
-text_box = driver.find_element(by=By.NAME, value="my-text")
-submit_button = driver.find_element(by=By.CSS_SELECTOR, value="button")
-
+text_box = driver.find_element(By.NAME, "my-text")
 text_box.send_keys("Selenium")
-submit_button.click()
 
-message = driver.find_element(by=By.ID, value="message")
-value = message.text
-print(value)
+driver.find_element(By.CSS_SELECTOR, "button").click()
 
-assert value == "Received!"
+message = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, "message"))
+)
+
+print(message.text)
+
+assert message.text == "Received!"
 driver.quit()
